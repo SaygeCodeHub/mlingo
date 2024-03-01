049dd6e3bffbef08a5e09d92b4dae880ccbc1ff9
vaishnavichafle <vaishnavic16@gmail.com>
2024-03-01 20:09:14 +0530

    MA-04: Merged.

--- lib/screens/login/registeration.dart
+++ lib/screens/login/registeration.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mlingo/configs/app_color.dart';
 import 'package:mlingo/configs/app_dimensions.dart';
